b01a93727872459a6ecaa71bd6692f416d0c353f
Przemek Maciolek <pmaciolek@sumologic.com>
2020-03-02 11:26:05 +0100

    Include resource labels in Zipkin export

--- translator/trace/spandata/protospan_to_spandata.go
+++ translator/trace/spandata/protospan_to_spandata.go
@@ -18,6 +18,7 @@ package spandata
 import (
 	"errors"
 	"time"
+	resourcepb "github.com/census-instrumentation/opencensus-proto/gen-go/resource/v1"
 
 	tracepb "github.com/census-instrumentation/opencensus-proto/gen-go/trace/v1"
 	"github.com/golang/protobuf/ptypes/timestamp"
@@ -29,7 +30,8 @@ import (
 var errNilSpan = errors.New("expected a non-nil span")
 
 // ProtoSpanToOCSpanData transforms a protobuf span into the equivalent trace.SpanData one.
-func ProtoSpanToOCSpanData(span *tracepb.Span) (*trace.SpanData, error) {
+// When resource is not nil, then its labels are attached to span attributes
+func ProtoSpanToOCSpanData(span *tracepb.Span, resource *resourcepb.Resource) (*trace.SpanData, error) {
 	if span == nil {
 		return nil, errNilSpan
 	}
@@ -50,7 +52,7 @@ func ProtoSpanToOCSpanData(span *tracepb.Span) (*trace.SpanData, error) {
 		StartTime:       timestampToTime(span.StartTime),
 		EndTime:         timestampToTime(span.EndTime),
 		Name:            derefTruncatableString(span.Name),
-		Attributes:      protoAttributesToOCAttributes(span.Attributes),
+		Attributes:      protoAttributesToOCAttributes(span.Attributes, resource),
 		Links:           protoLinksToOCLinks(span.Links),
 		Status:          protoStatusToOCStatus(span.Status),
 		SpanKind:        protoSpanKindToOCSpanKind(span.Kind),
@@ -136,12 +138,19 @@ func protoLinkTypeToOCLinkType(lt tracepb.Span_Link_Type) trace.LinkType {
 	}
 }
 
-func protoAttributesToOCAttributes(attrs *tracepb.Span_Attributes) map[string]interface{} {
+func protoAttributesToOCAttributes(attrs *tracepb.Span_Attributes, resource *resourcepb.Resource) map[string]interface{} {
 	if attrs == nil {
 		return nil
 	}
 
 	ocAttrsMap := make(map[string]interface{})
+
+	if resource != nil {
+		for key, value := range resource.Labels {
+			ocAttrsMap[key] = value
+		}
+	}
+
 	if len(attrs.AttributeMap) == 0 {
 		return ocAttrsMap
 	}
@@ -160,6 +169,7 @@ func protoAttributesToOCAttributes(attrs *tracepb.Span_Attributes) map[string]in
 			ocAttrsMap[key] = derefTruncatableString(value.StringValue)
 		}
 	}
+
 	return ocAttrsMap
 }
 
